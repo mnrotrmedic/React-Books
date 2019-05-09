36babac1a49c37d54f4701e6f965d71ac7f36a05
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-05-09 13:04:21 -0500

    stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Search from "./pages/Search/search"
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Search} />
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ client/src/pages/Search/search.js
@@ -0,0 +1,23 @@
+import { Col, Row, Container } from react - bootstrap
+
+class Search extends Component {
+
+
+
+
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>search.js</h1>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+
+}
+
+export default Search
